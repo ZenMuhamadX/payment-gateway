972ae30251db1c234e3156317ff45312001b33c0
muhamadzen669 <muhamadzen669@gmail.com>
2024-08-04 04:49:20 +0000

    test

--- src/route/hooks.ts
+++ src/route/hooks.ts
@@ -5,8 +5,8 @@ import { response } from '../config/response'
 import { Context } from 'hono'
 const route = new Hono()
 
-route.post('/', (c: Context) => {
-	const body = c.req.json()
+route.post('/', async (c: Context) => {
+	const body = await c.req.json()
 	console.log(body)
 	return response(c, null, 200, 'Hooks succes', { body })
 })
