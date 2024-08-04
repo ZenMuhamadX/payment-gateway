7a367016e1775db13736f4afead63f6bcbb59923
muhamadzen669 <muhamadzen669@gmail.com>
2024-08-04 11:59:01 +0000

    ok

--- src/controllers/hooks/hook.ts
+++ src/controllers/hooks/hook.ts
@@ -3,5 +3,6 @@ import { response } from '../../config/response'
 
 export const webHook = async (c: Context) => {
 	const body = await c.req.json()
+	console.log(body);
 	return response(c, null, 200, 'Hooks recived', { hooks: body })
 }
