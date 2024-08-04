84bfd5f47ba425815d2cf06430d57acee6141d04
muhamadzen669 <muhamadzen669@gmail.com>
2024-08-04 07:10:19 +0000

    ok

--- src/controllers/checkStatusPayment.c.ts
+++ src/controllers/checkStatusPayment.c.ts
@@ -36,7 +36,7 @@ export const checkStatusPayment = async (c: Context) => {
 	} catch (error) {
 		if (error instanceof Error) {
 			const midtransErr = error as MidtransError
-			const statusCodeMidtrans = midtransErr.httpStatusCode
+			const statusCodeMidtrans = midtransErr.rawHttpClientData
 			return response(
 				c,
 				midtransErr.ApiResponse.status_message,
