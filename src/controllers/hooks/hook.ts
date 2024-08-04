3fd487858227014ff6056f86c192e651a6446a36
muhamadzen669 <muhamadzen669@gmail.com>
2024-08-04 12:05:30 +0000

    interface

--- src/controllers/hooks/hook.ts
+++ src/controllers/hooks/hook.ts
@@ -1,8 +1,24 @@
 import { Context } from 'hono'
 import { response } from '../../config/response'
 
+interface hookFromMidtrans {
+	transaction_time: string
+	transaction_status: string
+	transaction_id: string
+	status_message: string
+	status_code: string
+	signature_key: string
+	settlement_time: string
+	payment_type: string
+	order_id: string
+	merchant_id: string
+	gross_amount: string
+	fraud_status: string
+	currency: string
+}
+
 export const webHook = async (c: Context) => {
-	const body = await c.req.json()
-	console.log(body);
-	return response(c, null, 200, 'Hooks recived', { hooks: body })
+	const hookBody: hookFromMidtrans = await c.req.json()
+	console.log(hookBody)
+	return response(c, null, 200, 'Hooks recived', { hooks:hookBody })
 }
