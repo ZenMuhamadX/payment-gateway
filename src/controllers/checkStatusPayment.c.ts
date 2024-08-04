b26f71a5c9a7d5a65e602e076470d8039339bf52
muhamadzen669 <muhamadzen669@gmail.com>
2024-08-04 04:14:10 +0000

    test

--- src/controllers/checkStatusPayment.c.ts
+++ src/controllers/checkStatusPayment.c.ts
@@ -1,6 +1,18 @@
 import { Context } from 'hono'
+import { snap } from '../lib/snap.lib'
+import { response } from '../config/response'
 
-export const checkStatusPayment = (c: Context) => {
+export const checkStatusPayment = async (c: Context) => {
+	// cek status pembayaran
 	const { orderID } = c.req.query()
-	return c.json(orderID)
+	if (!orderID) {
+		return response(c, 'require orderID in param', 400, 'bad request', null)
+	}
+	try {
+		const statusTx = await snap.transaction.status(orderID)
+		return response(c, null, 200, 'success', statusTx)
+	} catch (error) {
+		console.error(error)
+		return response(c, null, 500, 'internal server error', null)
+	}
 }
