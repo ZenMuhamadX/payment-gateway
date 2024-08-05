4749265fb42af5a7166639eb053a9572fe01d69f
muhamadzen669 <muhamadzen669@gmail.com>
2024-08-05 12:25:43 +0000

    test

--- src/controllers/hooks/hook.ts
+++ src/controllers/hooks/hook.ts
@@ -10,28 +10,44 @@ export const handleWebhook = async (c: Context) => {
 	try {
 		const payload: MidtransWebhookPayload = await c.req.json()
 
-		const signatureKey = payload.signature_key
+		const {
+			signature_key,
+			order_id,
+			status_code,
+			gross_amount,
+			transaction_status,
+			transaction_time,
+		} = payload
 		const isSignatureValid = validateMidtransSignature({
-			signatureKey,
-			orderId: payload.order_id,
-			statusCode: payload.status_code,
-			grossAmount: payload.gross_amount,
+			signatureKey: signature_key,
+			orderId: order_id,
+			statusCode: status_code,
+			grossAmount: gross_amount,
 		})
 
-		if (isSignatureValid) {
+		if (!isSignatureValid) {
+			return response(c, null, 400, 'Invalid Signature', null)
+		}
+
+		if (payload.transaction_status === 'capture') {
+			console.log('Transaction is captured')
+			if (payload.fraud_status === 'accept') {
+				console.log('Transaction is fraud')
+				return response(c, null, 200, 'Webhook recived and valid', null)
+			} return
 			sendTxData({
 				serverKey,
-				signatureKey,
-				orderId: payload.order_id,
-				transactionId: payload.transaction_status,
-				time: payload.transaction_time,
-				transactionStatus: payload.transaction_status,
-				grossAmount: payload.gross_amount,
+				signatureKey: signature_key,
+				orderId: order_id,
+				transactionId: transaction_status,
+				time: transaction_time,
+				transactionStatus: transaction_status,
+				grossAmount: gross_amount,
 			})
-			return response(c, null, 200, 'Webhook Verified', { signatureKey })
-		} else {
-			return response(c, null, 400, 'Webhook Not Verified')
 		}
+
+		// Kirim respons 200 OK jika tidak ada masalah
+		return response(c, null, 200, 'OK', null)
 	} catch (error) {
 		console.error('Error in handleWebhook:', error)
 		return response(c, null, 500, 'Internal Server Error', null)
