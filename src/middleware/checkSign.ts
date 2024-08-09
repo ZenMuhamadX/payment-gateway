d6ffdc9586f7c3a6ac433b84e3059670e51e1521
muhamadzen669 <muhamadzen669@gmail.com>
2024-08-09 09:10:27 +0000

    TEST

--- src/middleware/checkSign.ts
+++ src/middleware/checkSign.ts
@@ -10,10 +10,11 @@ interface requestClient {
 
 export const checkSign = async (c: Context) => {
 	const clientData: requestClient = await c.req.json()
+	const secretKey = process.env.JWT_SECRET_KEY
 	const signatureFrontEnd = clientData.signature
 	const isValidSign = crypto
 		.createHash('sha256')
-		.update(clientData.id_produk + clientData.username + clientData.email)
+		.update(clientData.username + clientData.email + secretKey)
 		.digest('hex')
 	console.log(clientData)
 	console.log(isValidSign)
